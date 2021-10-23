5c9b28f0063f0f6df175e77ce92b65949b518f4e
Sylwester Bąkowski <s_bakowski@ms.wwsi.edu.pl>
2021-10-23 16:49:43 +0200

    connect with database

---
+++ Models/FilmModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB2.Models
+{
+    
+
+    public class FilmModel
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+    }
+}
